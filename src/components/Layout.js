3a4fe32bf3a8fff3622bed7a615cff3ce881e0c2
Jess Zanewich <jessica.zanewich@gmail.com>
2019-11-29 15:50:27 -0700

    start layout styling and add layout to main design

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,12 @@
 import React from "react"
+import layoutStyles from "./Layout.module.css"
 
-const Layout = (props) => {
-  return <div></div>
+const Layout = props => {
+  return (
+    <div className={layoutStyles.container}>
+      <div>{props.children}</div>
+    </div>
+  )
 }
+
+export default Layout

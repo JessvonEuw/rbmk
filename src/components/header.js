64683d0104c2b810b27a84a28c2e37b2c334520d
Zanewich, Jessica <jzanewich@criticalmass.com>
2020-05-24 15:57:16 -0600

    add band images and adjust sizing. insert logo on main page

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,66 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import headerStyles from "./header.module.scss"
 import logo from "../images/logo.svg"
 import Icon from "../images/headerIcons"
 import BurgerMenu from "./burger"
 
-
-
 const Header = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   function IsHomePage() {
-    if (window.location.pathname !== '/') {
-        return (
-          <div className={headerStyles.centerSection}>
+    if (window.location.pathname !== "/") {
+      return (
+        <div className={headerStyles.centerSection}>
           <Link className={headerStyles.logoLinks} to="/">
             <img src={logo} alt="RBMK"></img>
           </Link>
         </div>
-        );
+      )
     } else {
-        return null;
+      return null
     }
-}
-    return (
-        <div className={headerStyles.header}>
-            <div className={headerStyles.leftSection}>
-                <div className={headerStyles.socialsIcons}>
-                    <ul className={headerStyles.list}>
-                        <li className={headerStyles.listItem}>
-                                <a className={headerStyles.links} href="https://www.youtube.com/channel/UCEz4ejRyIzClwBmBZmV3htg">
-                                  <Icon name="youtube" />
-                                </a>
-                            </li>
-                            <li className={headerStyles.listItem}>
-                                <a className={headerStyles.links} href="https://www.instagram.com/rbmk.band/">
-                                  <Icon name="instagram" />
-                                </a>
-                            </li>
-                            <li className={headerStyles.listItem}>
-                              <a className={headerStyles.links} href="https://www.facebook.com/RBMK-Band-114374710250620/">
-                                <Icon name="facebook" />                               
-                              </a> 
-                            </li>
-                            <li className={headerStyles.listItem}>
-                                <a className={headerStyles.links} href="https://soundcloud.com/user-417592303">
-                                  <Icon name="soundcloud" />
-                                </a>
-                            </li>
-                        </ul>
-                </div>
-            </div>
-            <IsHomePage />
+  }
+  return (
+    <div className={headerStyles.header}>
+      <div className={headerStyles.leftSection}>
+        <div className={headerStyles.socialsIcons}>
+          <ul className={headerStyles.list}>
+            <li className={headerStyles.listItem}>
+              <a
+                className={headerStyles.links}
+                href="https://www.youtube.com/channel/UCEz4ejRyIzClwBmBZmV3htg"
+              >
+                <Icon name="youtube" />
+              </a>
+            </li>
+            <li className={headerStyles.listItem}>
+              <a
+                className={headerStyles.links}
+                href="https://www.instagram.com/rbmk.band/"
+              >
+                <Icon name="instagram" />
+              </a>
+            </li>
+            <li className={headerStyles.listItem}>
+              <a
+                className={headerStyles.links}
+                href="https://www.facebook.com/RBMK-Band-114374710250620/"
+              >
+                <Icon name="facebook" />
+              </a>
+            </li>
+            <li className={headerStyles.listItem}>
+              <a
+                className={headerStyles.links}
+                href="https://soundcloud.com/user-417592303"
+              >
+                <Icon name="soundcloud" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <IsHomePage />
       <div className={headerStyles.rightSection}>
         <nav>
           <ul className={headerStyles.list}>
@@ -60,22 +70,21 @@ const Header = () => {
               </Link>
             </li>
             <li className={headerStyles.listItem}>
-              <Link className={headerStyles.links} to="/bookUs">
+              <Link className={headerStyles.links} to="/contact">
                 Book Us
               </Link>
             </li>
           </ul>
         </nav>
       </div>
-      <div className = {headerStyles.hamburgerMenu}>
-            <a onClick={() => setOpen(!open)}><Icon name="ham" /></a>
-            {open && <BurgerMenu />}
+      <div className={headerStyles.hamburgerMenu}>
+        <div onClick={() => setOpen(!open)}>
+          <Icon name="ham" />
+        </div>
+        {open && <BurgerMenu />}
       </div>
     </div>
-      )
+  )
 }
 
 export default Header
-
-
-
---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Layout from "../components/layout"
+import Section from "../components/Section"
+import ContactStyles from "./contact.module.scss"
+
+const aboutPage = () => {
+  return (
+    <Layout>
+      <div className={ContactStyles.content}>
+        <Section>
+          <h1 className={ContactStyles.header}>Talk to Us</h1>
+          <p className={ContactStyles.title}>Email</p>
+          <p className={ContactStyles.text}>info@rbmkband.com</p>
+          <p className={ContactStyles.title}>Phone</p>
+          <p className={ContactStyles.text}>(555) 555 5555</p>
+        </Section>
+      </div>
+    </Layout>
+  )
+}
+export default aboutPage

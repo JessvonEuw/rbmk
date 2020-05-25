c4bfe475ab68e6390697a6b4863a5282c2c86a47
Zanewich, Jessica <jzanewich@criticalmass.com>
2020-05-25 15:41:26 -0600

    image optimizations

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
+const path = require(`path`)
 
 module.exports = {
   /* Your site config here */
@@ -15,5 +16,14 @@ module.exports = {
         includePaths: ["src/styles"],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 }
